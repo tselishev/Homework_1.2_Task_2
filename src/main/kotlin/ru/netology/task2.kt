ac3319dda2957f0e78505fe3e44243cb45b9d5b7
tselishev <82764755+tselishev@users.noreply.github.com>
2021-05-09 16:01:59 +0500

    Initial commit

---
+++ src/main/kotlin/ru/netology/task2.kt
@@ -0,0 +1,11 @@
+package ru.netology
+
+fun main() {
+    val likes = 131
+
+    if (likes % 2 == 0){
+        println ("Понравилось $likes людям")
+    } else {
+        println ("Понравилось $likes человеку")
+    }
+}
